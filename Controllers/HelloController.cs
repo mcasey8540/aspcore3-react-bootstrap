910dd8a5709b2b378c5ed972235296fb316c36c8
Casey <caseymt@richva.ci.richmond.va.us>
2019-10-04 10:53:31 -0400

    Add MVC controller Hello with Index method and view

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dit_react_bootstrap.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
